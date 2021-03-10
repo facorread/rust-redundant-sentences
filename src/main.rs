b9db0086ccd66651c457dd3c382cf3ed4446d62a
Fabio A. Correa Duran <facorread@gmail.com>
2021-03-10 09:20:41 -0500

    Read from files instead of stdin

--- src/main.rs
+++ src/main.rs
@@ -37,59 +37,156 @@ impl std::fmt::Display for Sentence {
     }
 }
 
-fn main() {
-    let mut sentences: HashMap<String, Sentence> = HashMap::new();
+static TRY_AGAIN_MSG: &str = "Please review this problem. Before trying again, make sure input/ is a valid directory and your files are in txt format.";
+static USAGE_MSG: &str =
+    "Before starting rust-redundant-sentences, save your plain text to the input/ directory. Only txt files are accepted.";
 
-    'new_page_of_text: loop {
-        println!("Paste a page of text here.\nEnter EOF when you are done with the page, or Ctrl + C to close this program:");
-        let mut mode = Mode::Header;
-        use std::io::BufRead;
-        let stdin_raw = std::io::stdin();
-        let stdin = stdin_raw.lock();
-        for line_result in stdin.lines() {
-            if let Ok(line) = line_result {
-                match mode {
-                    Mode::Header => match line.as_str() {
-                        "Page" => mode = Mode::TextBody,
-                        "EOF" => break 'new_page_of_text,
-                        _ => {
-                            println!("{}", line);
-                        }
-                    },
-                    Mode::TextBody => {
-                        if line == "EOF" {
-                            continue 'new_page_of_text;
-                        }
-                        line.split('.').for_each(|sentence| {
-                            if sentence.len() > 40 {
-                                let reduced: String =
-                                    sentence.chars().filter(|c| c.is_alphanumeric()).collect();
-                                sentences
-                                    .entry(reduced)
-                                    .or_insert_with(|| Sentence {
-                                        text: String::from(sentence.trim()),
-                                        count: 0,
-                                    })
-                                    .count += 1;
+fn create_output_file() -> Option<std::fs::File> {
+    match std::fs::File::create("report.txt") {
+        Ok(out_file) => Some(out_file),
+        Err(e) => {
+            println!("Could not create report.txt: {}\n{}", e, TRY_AGAIN_MSG);
+            None
+        }
+    }
+}
+
+fn get_input_path() -> Option<std::path::PathBuf> {
+    let input_path = std::path::PathBuf::from("input");
+    if input_path.exists() {
+        if input_path.is_dir() {
+            Some(input_path)
+        } else {
+            println!("input/ is not a directory. {}", TRY_AGAIN_MSG);
+            None
+        }
+    } else {
+        // The input directory does not exist. Try to create it.
+        if let Err(e) = std::fs::create_dir(input_path) {
+            println!("Could not create the input/ directory:\n{}\nPlease review this problem. Then, manually create the input/ directory.", e);
+        }
+        println!("{}", USAGE_MSG);
+        None
+    }
+}
+
+fn open_input_file(
+    result_dir_entry: std::result::Result<std::fs::DirEntry, std::io::Error>,
+) -> Option<std::fs::File> {
+    let txt_ext = Some(std::ffi::OsStr::new("txt"));
+    match result_dir_entry {
+        Ok(dir_entry) => {
+            let file_path = dir_entry.path();
+            let os_string_file_name = dir_entry.file_name();
+            let file_name = os_string_file_name.to_string_lossy();
+            match dir_entry.file_type() {
+                Ok(file_type) => {
+                    if file_type.is_file() {
+                        if file_path.extension() == txt_ext {
+                            match std::fs::File::open(file_path) {
+                                Ok(input_file_unbuffered) => Some(input_file_unbuffered),
+                                Err(e) => {
+                                    println!(
+                                        "Error opening input/{}:\n{}\n{}",
+                                        file_name, e, TRY_AGAIN_MSG
+                                    );
+                                    None
+                                }
                             }
-                        });
+                        } else {
+                            println!(
+                                "Error: input/{} is not a txt file.\n{}",
+                                file_name, TRY_AGAIN_MSG
+                            );
+                            None
+                        }
+                    } else {
+                        println!(
+                            "Error: input/{} is not a regular file.\n{}",
+                            file_name, TRY_AGAIN_MSG
+                        );
+                        None
                     }
+                }
+                Err(e) => {
+                    println!(
+                        "Error when checking the type of input/{}:\n{}\n{}",
+                        file_name, e, TRY_AGAIN_MSG
+                    );
+                    None
                 }
             }
         }
-    }
-    let mut sentences: Vec<_> = sentences.values().collect();
-    sentences.sort_unstable_by_key(|sentence| -sentence.count);
-    println!("Total sentences: {}", sentences.len());
-    let nontrivial = |x: &&Sentence| !x.text.starts_with("Collapse Subdiscussion");
-    let repeated = |x: &&Sentence| nontrivial(x) && x.count > 1;
-    if sentences.iter().any(repeated) {
-        for sentence in sentences.into_iter().filter(repeated) {
-            println!("{}", sentence);
+        Err(e) => {
+            println!(
+                "Error with an entry in the input/ directory:\n{}\n{}",
+                e, TRY_AGAIN_MSG
+            );
+            None
         }
-    } else {
-        for sentence in sentences.into_iter().filter(nontrivial) {
-            println!("{}", sentence);
+    }
+}
+
+fn main() {
+    use std::io::prelude::*;
+    let mut sentences: HashMap<String, Sentence> = HashMap::new();
+    if let Some(out_file_unbuffered) = create_output_file() {
+        let out_file = std::io::BufWriter::new(out_file_unbuffered);
+        if let Some(input_path) = get_input_path() {
+            for result_dir_entry in
+                std::fs::read_dir(input_path).expect("Listing the contents of input/")
+            {
+                match open_input_file(result_dir_entry) {
+                    Some(input_file_unbuffered) => {
+                        let mut mode = Mode::Header;
+                        let input_file = std::io::BufReader::new(input_file_unbuffered);
+                        for line_result in input_file.lines() {
+                            if let Ok(line) = line_result {
+                                match mode {
+                                    Mode::Header => {
+                                        if line == "Page" {
+                                            mode = Mode::TextBody;
+                                        }
+                                    }
+                                    Mode::TextBody => {
+                                        line.split('.').for_each(|sentence| {
+                                            if sentence.len() > 40 {
+                                                let reduced: String = sentence
+                                                    .chars()
+                                                    .filter(|c| c.is_alphanumeric())
+                                                    .collect();
+                                                sentences
+                                                    .entry(reduced)
+                                                    .or_insert_with(|| Sentence {
+                                                        text: String::from(sentence.trim()),
+                                                        count: 0,
+                                                    })
+                                                    .count += 1;
+                                            }
+                                        });
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    None => return,
+                }
+            }
+            let mut sentences: Vec<_> = sentences.values().collect();
+            sentences.sort_unstable_by_key(|sentence| -sentence.count);
+            println!("Total sentences: {}", sentences.len());
+            let nontrivial = |x: &&Sentence| !x.text.starts_with("Collapse Subdiscussion");
+            let repeated = |x: &&Sentence| nontrivial(x) && x.count > 1;
+            if sentences.iter().any(repeated) {
+                for sentence in sentences.into_iter().filter(repeated) {
+                    println!("{}", sentence);
+                }
+            } else {
+                for sentence in sentences.into_iter().filter(nontrivial) {
+                    println!("{}", sentence);
+                }
+                println!("{}", USAGE_MSG);
+            }
         }
     }
 }
